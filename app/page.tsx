94e3f2183a5e238c7de8f63300213c1da06c604a
Movieguy3333 <Movieguy3333@gmail.com>
2026-01-14 15:29:42 -0500

    the start of git-together

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+const Page = () => {
+  return (
+    <section>
+      <h1 className="text-center">
+        The Hub for Every Dev <br />
+        Event You Mustn't Miss
+      </h1>
+      <p className="text-center mt-5">
+        Hackathon, Conferences, Workshops, and more
+      </p>
+    </section>
+  );
+};
+
+export default Page;
